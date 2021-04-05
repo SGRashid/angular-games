2e46374aa53ac752a20c911846850c67c58f5aa6
GabdullinSR <gabdullinsr@mail.skblab.ru>
2021-04-05 12:00:52 +0500

    Змейка поворачиваетя

--- projects/sergei-angular-snake/src/lib/sergei-angular-snake.component.ts
+++ projects/sergei-angular-snake/src/lib/sergei-angular-snake.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, OnDestroy, HostListener } from '@angular/core';
 import { interval, Subscription } from 'rxjs';
 
+// типы в отдельный файл!!
+
+type Direction = -1 | 0 | 1;
+
 // for tests
 const snake = [
   { x: 5, y: 5},
@@ -13,6 +17,7 @@ const snake = [
   selector: 'lib-snake',
   template: `
     <lib-grid [width]="20" [snake]="snake"></lib-grid>
+    <div style="color: white;" *ngIf="!isGameOn">PAUSE SCORE {{ score }}</div>
   `,
   styles: [
   ]
@@ -20,13 +25,35 @@ const snake = [
 export class SergeiAngularSnakeComponent implements OnInit, OnDestroy {
 
   snake: any;
+  score = 0;
   delay = 1000;
   isGameOn = true;
+  xDirection = 1;
+  yDirection = 0;
 
   subscription = new Subscription();
 
-  @HostListener('document:keydown.space') pauseOrStartGame($event): void {
-    this.isGameOn = !this.isGameOn;
+  @HostListener('document:keydown', ['$event']) onKeyDown(event: KeyboardEvent): void {
+    console.log(event);
+    if (event.code === 'Space') {
+      this.pauseOrStartGame();
+    }
+
+    if (event.code === 'ArrowUp') {
+      this.setDirection(0, -1);
+    }
+
+    if (event.code === 'ArrowDown') {
+      this.setDirection(0, 1);
+    }
+
+    if (event.code === 'ArrowLeft') {
+      this.setDirection(-1, 0);
+    }
+
+    if (event.code === 'ArrowRight') {
+      this.setDirection(1, 0);
+    }
   }
 
   constructor() { }
@@ -53,8 +80,20 @@ export class SergeiAngularSnakeComponent implements OnInit, OnDestroy {
     }
 
     const len = this.snake.length;
-    const newPoint = {x: this.snake[len - 1].x + 1, y: this.snake[len - 1].y};
+    const newPoint = {
+      x: this.snake[len - 1].x + this.xDirection,
+      y: this.snake[len - 1].y + this.yDirection
+    };
     this.snake = [...this.snake.slice(1, len), newPoint];
+  }
+
+  pauseOrStartGame(): void {
+    this.isGameOn = !this.isGameOn;
+  }
+
+  setDirection(x: Direction, y: Direction): void {
+    this.xDirection = x;
+    this.yDirection = y;
   }
 
 }
